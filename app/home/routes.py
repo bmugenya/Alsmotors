e94bf9b0d6e978901704bb25e879f9335f982193
bmugenya <bmugenya26@gmail.com>
2023-01-17 20:10:06 +0300

    update

--- app/home/routes.py
+++ app/home/routes.py
@@ -4,7 +4,7 @@ from flask_login import login_required
 from jinja2 import TemplateNotFound
 from app.authentication.models import Car
 from app import db,login_manager
-
+from sqlalchemy import or_
 import cloudinary.uploader
 
 
@@ -16,6 +16,39 @@ def index():
 @blueprint.route('/inventory')
 def inventory():
     cars =  Car.query.all()
+    return render_template('home/inventory.html',item_length="true",cars=cars, segment='index')
+
+
+
+@blueprint.route('/filter', methods=['POST'])
+def filter():
+
+    s_type = request.form['type']
+    brand = request.form['brand']
+    make = request.form['make']
+    # price = request.form['price']
+    models = request.form['models']
+    location = request.form['location']
+    fuel = request.form['fuel']
+    transmission = request.form['transmission']
+    cars = 'searching'
+
+
+    if brand:
+        cars =  Car.query.filter(Car.brand.like(brand))
+    if s_type:
+        cars =  Car.query.filter(Car.type.like(s_type))
+    if make:
+        cars =  Car.query.filter(Car.make.like(make))
+    if models:
+        cars =  Car.query.filter( Car.models.like(models))
+    if location:
+        cars =  Car.query.filter(Car.location.like(location))
+    if fuel:
+        cars =  Car.query.filter(Car.fuel.like(fuel))
+    if transmission:
+        cars =  Car.query.filter(Car.transmission.like(transmission))
+
     return render_template('home/inventory.html',cars=cars, segment='index')
  
 
@@ -28,12 +61,12 @@ def car():
 def add_car():
     """Register a new car"""
   
-    file = request.files['image']
-    upload_data = cloudinary.uploader.upload(file)
-    photo = upload_data['secure_url']
+    # file = request.files['image']
+    # upload_data = cloudinary.uploader.upload(file)
+    # photo = upload_data['secure_url']
     
     data = {
-        'image_url':photo,
+        # 'image_url':photo,
         'name' : request.form['name'],
         'type' : request.form['type'],
         'brand' : request.form['brand'],
