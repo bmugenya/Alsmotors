ee2a1d9f9582e341b0303be50ced2b56158e4daa
bmugenya <bmugenya26@gmail.com>
2023-04-17 18:39:06 +0300

    update

--- app/static/assets/components/vehicles/vehicles.js
+++ app/static/assets/components/vehicles/vehicles.js
@@ -91,9 +91,9 @@ class App extends Component {
     
 
   return html`${this.state.ready && html`
-    <div class="card card-body blur shadow-blur mx-3 mx-md-10 mt-n7" style="padding:0 !important;">
+    <div class="card card-body blur shadow-blur mx-3 mx-md-4 mt-n7" style="padding:0 !important;">
       <div class="container">
-        <form role="form" method="post" onSubmit=${searchVeh} style="display: flex;">
+        <form role="form" method="post" onSubmit=${searchVeh}>
         <div class="row">
           <div class="col-lg-12 mx-auto py-3">
             <div class="row">
@@ -101,13 +101,12 @@ class App extends Component {
             <h6>I WANT TO BUY</h6>
           </div>
           <div class="col-lg-6 text-end d-flex justify-content-center">
-            <button type="button" onClick=${({ target }) => this.setState({used:false})} class="btn btn-outline-warning ms-lg-1 me-lg-0 me-auto mt-lg-0 mt-2">BRAND NEW</button>
-            <button type="button" onClick=${({ target }) => this.setState({used:true})} class="btn btn-outline-warning  ms-lg-1 me-lg-0 me-auto mt-lg-0 mt-2">SECOND HAND</button>
+            <button type="button" class="btn btn-outline-warning  ms-lg-1 me-lg-0 me-auto mt-lg-0 mt-2">AL Siddique Motors</button>
           </div>
         </div>
       <div class="row">
         
-          <div class="col-md-2 position-relative">
+          <div class="col-lg-3 mt-lg-n2 mt-2">
             <div class="input-group input-group-dynamic">
                     <select class="select-css" onChange=${({ target }) => this.setState({location:target.value})} name="location">
                       <option value="">Location</option>
@@ -122,7 +121,26 @@ class App extends Component {
                 <input type="hidden" name="make" />
                 <input type="hidden" name="models" />
 
-                <div class="col-md-2 position-relative">
+                   <div class="col-lg-3 mt-lg-n2 mt-2">
+
+                  <div class="input-group input-group-dynamic">
+                    <select name="brand" onChange=${({ target }) => this.setState({brand:target.value})}  class="select-css">
+                      <option value="">All Brand</option>
+                      <option value="toyota">TOYOTA</option>
+                      <option value="nissan">NISSAN</option>
+                      <option value="mazda">MAZDA</option>
+                      <option value="subaru">SUBARU</option>
+                      <option value="isuzu">ISUZU</option>
+                      <option value="mistubishi">MISTUBISHI</option>
+                      <option value="honda">HONDA</option>
+                      <option value="bmw">BMW</option>
+                      <option value="benz">BENZ</option>
+                      <option value="other">OTHER</option>
+                    </select>
+                  </div>
+                </div>   
+
+                <div class="col-lg-3 mt-lg-n2 mt-2">
 
                   <div class="input-group input-group-dynamic">
                     <select name="category" onChange=${({ target }) => this.setState({category:target.value})} class="select-css">
@@ -136,57 +154,10 @@ class App extends Component {
 
                 </div>
 
-                <div class="col-md-2 position-relative">
-
-                  <div class="input-group input-group-dynamic">
-                    <select name="brand" onChange=${({ target }) => this.setState({brand:target.value})}  class="select-css">
-                      <option value="">All Models</option>
-                      <option value="toyota">TOYOTA</option>
-                      <option value="nissan">NISSAN</option>
-                      <option value="mazda">MAZDA</option>
-                      <option value="subaru">SUBARU</option>
-                      <option value="isuzu">ISUZU</option>
-                      <option value="mistubishi">MISTUBISHI</option>
-                      <option value="honda">HONDA</option>
-                      <option value="bmw">BMW</option>
-                      <option value="benz">BENZ</option>
-                      <option value="other">OTHER</option>
-                    </select>
-                  </div>
-                </div>
 
 
 
-                <div class="col-md-2 position-relative">
-
-                  <div class="input-group input-group-dynamic">
-                    <select class="select-css" name="fuel" onChange=${({ target }) => this.setState({fuel:target.value})} aria-label="First Name...">
-                      <option value="">Fuel Type</option>
-                      <option value="disel">DISEL</option>
-                      <option value="electric">ELECTRIC</option>
-                      <option value="petrol">PETROL</option>
-                      <option value="hybrid">HYBRID</option>
-                    </select>
-                  </div>
-
-                </div>
-
-
-                <div class="col-md-2 position-relative">
-
-                  <div class="input-group input-group-dynamic">
-                    <select class="select-css" name="transmission" onChange=${({ target }) => this.setState({transmission:target.value})} aria-label="First Name...">
-                      <option value="">Transmission</option>
-                      <option value="automatic">AUTOMATIC</option>
-                      <option value="manual">MANUAL</option>
-                      <option value="hybrid">HYBRID</option>
-                    </select>
-                  </div>
-
-                </div>
-
-
-                <div class="col-md-2">
+                <div class="col-lg-3 mt-lg-n2 mt-2">
 
                   <div class="input-group input-group-dynamic text-end">
                     <button type="submit" class="btn bg-gradient-warning">
